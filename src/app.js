fb9e8be38ba2fd72e217596b363b0f213adc99df
Vladimir Ionov <vawsan@mail.ru>
2019-10-06 21:58:02 +0300

    added front + webserver

--- src/app.js
+++ src/app.js
@@ -1,27 +1,103 @@
 const fs = require('fs');
 const path = require('path');
 const iconv = require('iconv-lite');
+const express = require('express');
+const multer  = require('multer');
 
 const { addFileToDb, calculateDays, convertToCsvFormat } = require('./lib');
 
-processFiles();
+const port = process.env.PORT || 3000;
 
-function processFiles() {
-    const filesList = fs.readdirSync('./../data', { withFileTypes: '*.txt'});
+const app = express();
+app.use(express.static('ui'));
+
+cleanDirs();
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, __dirname + '/input/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname.slice(0, file.originalname.indexOf('.')) + '_' + Date.now() + '.txt' );
+    }
+});
+  
+const upload = multer({ storage: storage });
+
+app.post('/', upload.array('logs'), (req, res, next) => {
+    const filesList = Array.from(req.files).map(file => file.destination + file.filename);
+    const resultFilePath = processFiles(filesList);
+
+    res.send({
+        url: resultFilePath
+    });
+});
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html');
+});
+
+app.get('/output/:name', (req, res) => {
+    const filePath = __dirname + req.url;
+    if (fs.existsSync(filePath)) {
+        res.download(filePath);
+    } else {
+        res.send({
+            error: 'Файл уже удален!'
+        });
+    }
+});
+
+app.listen(port, () => 
+    console.log(`http://localhost:${port}`)
+);
+
+function cleanDirs() {
+    const inputPath = path.resolve('input');
+    const outputPath = path.resolve('output');
+
+    fs.readdir(inputPath, (err, files) => {
+        deleteFiles(files.map(file => path.resolve('input', file)));
+    });
+    fs.readdir(outputPath, (err, files) => {
+        deleteFiles(files.map(file => path.resolve('output', file)));
+    });
+}
+
+function deleteFiles(fileList) {
+    if (fileList.length) {
+        fileList.forEach(file => {
+            fs.unlink(path.resolve(file), err => {
+                if (err) {
+                    console.log(err);
+                }
+            });
+        });
+    }
+}
+
+function processFiles(filesList) {
     const db = addAllFilestoDb(filesList);
     const resultsObj = calculateDays(db);
-
     const csvData = convertToCsvFormat(resultsObj);
+    const resultFileName = `output/result_${Date.now()}.csv`;
+    const resultFilePath = path.resolve(resultFileName);
+    fs.writeFileSync(resultFilePath, csvData.join('\n'));
 
-    fs.writeFileSync('result.csv', csvData.join('\n'));
+    deleteFiles(filesList);
+
+    setTimeout(() => {
+        deleteFiles([resultFilePath]);
+    }, 10 * 60 * 1000);
+    
+    return resultFileName;
 }
 
 function addAllFilestoDb(filesList) {
     const db = {};
 
     for(let i=0; i<filesList.length; i++) {
-        const filePath = path.resolve('./../data', filesList[i].name);
-        const rowsArr = readFileToArray(filePath);
+        const rowsArr = readFileToArray(filesList[i]);
         addFileToDb(db, rowsArr);
     }
     return db;
---
+++ src/ui/index.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var input = document.querySelector('#file');
+    var submit = document.querySelector('#submit');
+    var label = document.querySelector('#label');
+    var list = document.querySelector('#list');
+    var spinner = document.querySelector('#spinner');
+    var downloadBtn = document.querySelector('#download');
+    var alertNode = document.querySelector('#alert');
+
+    input.addEventListener('change', function (event) {
+        var filesSelected = input.files;
+        if (filesSelected.length) {
+            var fileNames = Array.from(filesSelected).map(item => item.name);
+            var liNodes = fileNames.map(item => `<li class="list-group-item">${item}</li>`).join('');
+            label.textContent = 'Файлы выбраны ↓';
+            list.innerHTML = liNodes;
+            list.classList.remove('hidden');
+            submit.classList.remove('hidden');
+            downloadBtn.classList.add('hidden');
+            alertNode.classList.add('hidden');
+            downloadBtn.href = '';
+        }
+    });
+    
+    submit.addEventListener('click', function (event) {
+        event.preventDefault();
+        spinner.classList.remove('hidden');
+        var filesSelected = input.files;
+        
+        sendData('/', filesSelected, function(response) {
+            spinner.classList.add('hidden');
+            list.innerHTML = '';
+            submit.classList.add('hidden');
+            downloadBtn.href = response.url;
+            downloadBtn.classList.remove('hidden');
+            alertNode.classList.remove('hidden');
+            label.textContent = 'Выбрать файлы';
+
+            setTimeout(function() {
+                alertNode.classList.add('hidden');
+                downloadBtn.classList.add('hidden');
+            }, 10 * 60 * 1000);
+        });
+    });
+});
+
+function sendData(url, files, callback) {
+    var formData  = new FormData();
+  
+    Array.from(files).forEach(file => {
+        formData.append('logs', file);
+    });
+  
+    fetch(url, {
+      method: 'POST',
+      body: formData
+    }).then(response => response.json())
+    .then(callback);
+}
